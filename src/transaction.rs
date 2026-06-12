a2a2389edf3fe45b63d24fb834cdc42e0106e554
inazrabuu <inazrabuu@gmail.com>
2026-06-12 16:58:45 +0700

    make public

--- src/transaction.rs
+++ src/transaction.rs
@@ -2,26 +2,23 @@ pub struct Transaction {
     pub hash: String,
     pub from: String,
     pub to: String,
-    pub amount: f64
+    pub amount: f64,
 }
 
 impl Transaction {
-    pub fn new(
-        hash: String,
-        from: String,
-        to: String,
-        amount: f64
-    ) -> Self{
-        Self { hash, from, to, amount }
+    pub fn new(hash: String, from: String, to: String, amount: f64) -> Self {
+        Self {
+            hash,
+            from,
+            to,
+            amount,
+        }
     }
 
     pub fn summary(&self) {
         println!(
             "Transaction {}: {} sent {} ETH to {}",
-            self.hash,
-            self.from,
-            self.amount,
-            self.to
+            self.hash, self.from, self.amount, self.to
         )
     }
 }

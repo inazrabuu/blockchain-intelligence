880d4f402686fe43a66da615572efe1d5b47daa5
inazrabuu <inazrabuu@gmail.com>
2026-06-16 14:54:53 +0700

    init

--- api-service/src/main.rs
+++ api-service/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Json,
+    Router
+};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct HealthResponse {
+    status: String
+}
+
+async fn health() -> Json<HealthResponse> {
+    Json(
+        HealthResponse {
+            status: String::from("Ok"),
+        }
+    )
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/health", get(health));
+
+    let listener = 
+        tokio::net::TcpListener::bind("0.0.0.0:3000")
+            .await
+            .unwrap();
+
+    println!("API listening on port 3000");
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
 }

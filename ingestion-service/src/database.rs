93399b83c9580cbaa4a0a54542ec3aab3f9f9581
inazrabuu <inazrabuu@gmail.com>
2026-06-16 13:08:22 +0700

    adding insert

--- ingestion-service/src/database.rs
+++ ingestion-service/src/database.rs
@@ -1,3 +1,4 @@
+use crate::transaction::Transaction;
 use sqlx::{PgPool, postgres::PgPoolOptions};
 
 pub async fn connect(database_url: &str) -> Result<PgPool, sqlx::Error> {
@@ -5,4 +6,30 @@ pub async fn connect(database_url: &str) -> Result<PgPool, sqlx::Error> {
       .max_connections(5)
       .connect(database_url)
       .await
+}
+
+pub async fn insert_transaction(
+  pool: &PgPool,
+  transaction: &Transaction,
+) -> Result<(), sqlx::Error> {
+  sqlx::query(
+    r#"
+    INSERT INTO transactions (
+      hash,
+      "from",
+      "to",
+      amount,
+      timestamp
+    ) VALUES ($1, $2, $3, $4, $5)
+    "#
+  )
+  .bind(&transaction.hash)
+  .bind(&transaction.from)
+  .bind(&transaction.to)
+  .bind(&transaction.amount)
+  .bind(&transaction.timestamp)
+  .execute(pool)
+  .await?;
+
+  Ok(())
 }

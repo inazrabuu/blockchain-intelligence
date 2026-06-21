aab42fe19b1fda1c0e6c314b0d49d6b3a262e06f
inazrabuu <inazrabuu@gmail.com>
2026-06-21 18:59:11 +0700

    add redis sub

---
+++ api-service/src/redis_sub.rs
@@ -0,0 +1,31 @@
+use redis::{AsyncCommands, RedisResult};
+use futures_util::StreamExt;
+
+pub async fn connect(
+    url: &str
+) -> redis::RedisResult<redis::Client> {
+    redis::Client::open(url)
+}
+
+pub async fn subscribe_transactions(
+    client: &redis::Client,
+) -> redis::RedisResult<()> {
+    let mut pubsub = client
+        .get_async_pubsub()
+        .await?;
+
+    pubsub
+        .subscribe("transaction_events")
+        .await?;
+    println!("{}", "Subscribed to tranasction_events");
+
+    let mut stream = pubsub.on_message();
+
+    while let Some(message) = stream.next().await {
+        let payload: String = message.get_payload()?;
+
+        println!("Received: {}", "payload");
+    }
+
+    Ok(())
+}

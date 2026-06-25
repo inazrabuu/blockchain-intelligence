178af744f41f34f1f09de1effa86874a851e5d7d
inazrabuu <inazrabuu@gmail.com>
2026-06-25 20:30:21 +0700

    adding analytics struct

---
+++ api-service/src/analytics.rs
@@ -0,0 +1,30 @@
+use shared::transaction::Transaction;
+
+#[derive(Debug,Default)]
+pub struct AnalyticsState {
+    pub total_transaction: u64,
+    pub total_volume: f64,
+    pub largest_transaction: Option<Transaction>,
+    pub whale_transaction: u64
+}
+
+impl AnalyticsState {
+    pub fn process_transaction(
+      &mut self,
+      tx: &Transaction
+    ) {
+        self.total_transaction += 1;
+        self.total_volume += tx.amount;
+
+        if tx.amount > 10_000.0 {
+            self.whale_transaction += 1;
+        }
+
+        match &self.largest_transaction {
+            Some(current) if current.amount >= tx.amount => {}
+            _ => {
+                self.largest_transaction = Some(tx.clone());
+            }
+        }
+    }
+}

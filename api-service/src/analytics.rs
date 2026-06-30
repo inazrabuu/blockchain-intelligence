83509457d6e8d96e1bc8a4b17252b6dd90fb57c2
inazrabuu <inazrabuu@gmail.com>
2026-06-30 20:02:47 +0700

    adding rolling window-transaction-volume

--- api-service/src/analytics.rs
+++ api-service/src/analytics.rs
@@ -2,13 +2,24 @@ use shared::transaction::Transaction;
 
 use std::sync::Arc;
 use tokio::sync::{broadcast, RwLock};
+use std::collections::VecDeque;
+
+const ROLLING_WINDOW_SECONDS: i64 = 60;
 
 #[derive(Debug,Clone,serde::Serialize)]
 pub struct AnalyticsSnapshot {
     pub total_transaction: u64,
     pub total_volume: f64,
     pub largest_transaction: Option<Transaction>,
-    pub whale_transaction: u64
+    pub whale_transaction: u64,
+    pub rolling_transaction_count: usize,
+    pub rolling_volume: f64
+}
+
+#[derive(Debug,Clone)]
+struct RollingTransaction {
+    pub timestamp: i64,
+    pub amount: f64
 }
 
 #[derive(Debug,Default,Clone)]
@@ -16,7 +27,10 @@ pub struct AnalyticsState {
     pub total_transaction: u64,
     pub total_volume: f64,
     pub largest_transaction: Option<Transaction>,
-    pub whale_transaction: u64
+    pub whale_transaction: u64,
+    pub recent_transactions: VecDeque<RollingTransaction>,
+    pub rolling_transaction_count: usize,
+    pub rolling_volume: f64
 }
 
 impl AnalyticsState {
@@ -37,6 +51,20 @@ impl AnalyticsState {
                 self.largest_transaction = Some(tx.clone());
             }
         }
+
+        self.recent_transactions.push_back(
+            RollingTransaction { 
+                timestamp: tx.timestamp, 
+                amount: tx.amount
+            }
+        );
+
+        self.rolling_transaction_count += 1;
+        self.rolling_volume += tx.amount;
+
+        self.evict_old_transaction(tx.timestamp);
+
+
     }
 
     pub fn snapshot(&self) -> AnalyticsSnapshot {
@@ -44,7 +72,23 @@ impl AnalyticsState {
             total_transaction: self.total_transaction, 
             total_volume: self.total_volume, 
             largest_transaction: self.largest_transaction.clone(), 
-            whale_transaction: self.whale_transaction 
+            whale_transaction: self.whale_transaction ,
+            rolling_transaction_count: self.rolling_transaction_count,
+            rolling_volume: self.rolling_volume
+        }
+    }
+
+    pub fn evict_old_transaction(&mut self, current_timestamp: i64) {
+        while let Some(tx) = self.recent_transactions.front() {
+            let age = current_timestamp - tx.timestamp;
+            if age <= ROLLING_WINDOW_SECONDS {
+                break;
+            }
+
+            if let Some(expired) = self.recent_transactions.pop_front() {
+                self.rolling_transaction_count -= 1;
+                self.rolling_volume -= expired.amount;
+            }
         }
     }
 }

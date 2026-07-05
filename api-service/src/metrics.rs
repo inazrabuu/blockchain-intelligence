92b566942ca9124416179527c17f0915c4500c98
inazrabuu <inazrabuu@gmail.com>
2026-07-05 19:56:03 +0700

    adding metrics

--- api-service/src/metrics.rs
+++ api-service/src/metrics.rs
@@ -1,5 +1,10 @@
-use metrics::counter;
+use metrics::{counter, histogram};
 
-pub fn http_request() {
+pub fn record_http_request() {
     counter!("blockchain_http_requests_total").increment(1);
+}
+
+pub fn record_http_duration(duration: f64) {
+    histogram!("blockchain_http_requests_duration_seconds")
+        .record(duration);
 }

337e6fcac66e7b4973cbf6f4b56e8a3b56037158
inazrabuu <inazrabuu@gmail.com>
2026-08-25 16:38:54 +0700

    load test api read

---
+++ load-test/api-read.js
@@ -0,0 +1,17 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export const options = {
+  vus: 1,
+  duration: "30s",
+};
+
+export default function () {
+  const response = http.get("http://localhost:3000/transactions");
+
+  check(response, {
+    "status is 200": (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
